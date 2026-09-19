5c3b784b1877c1882fa3cb9fedca46806fee5860
roni-discord6666 <aaronzerubia@gmail.com>
2026-09-20 01:46:30 +0200

    Use canonical www domain for auth

--- dist/env.js
+++ dist/env.js
@@ -1,5 +1,5 @@
 window.__STOA_ENV__ = {
-  SITE_URL: 'https://stoa-coaching.fr',
+  SITE_URL: 'https://www.stoa-coaching.fr',
   SUPABASE_URL: 'https://xriaekbrswqmosecjmhm.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_QBYj3ffRXyWI65qWOgt3Xg_d0b_9xb2',
 };
